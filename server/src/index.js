8078135bb277fad509816ab8e1e40fe50b285d5c
benf3632 <benf3632@gmail.com>
2020-04-26 20:34:11 +0300

    added 'host' route to get the host details

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var os = require('os');
 var app = express();
 
 app.listen(3000, () => {
@@ -8,3 +9,27 @@ app.listen(3000, () => {
 app.get('/url', (req, res, next) => {
 	res.json(['Tony', 'Lisa', 'Michael', 'Ginger', 'Food']);
 });
+
+app.get('/host', (req, res, next) => {
+	const hostname = os.hostname();
+	const ip = getIp();
+	res.json({ hostname, ip });
+});
+
+const getIp = () => {
+	const ifaces = os.networkInterfaces();
+	let ip = '';
+	let stop = false;
+	Object.keys(ifaces).forEach(ifname => {
+		if (stop) return;
+		ifaces[ifname].forEach(iface => {
+			if ('IPv4' !== iface.family || iface.internal !== false) return;
+			if (iface.address.includes('192.168.1.')) {
+				ip = iface.address;
+				stop = true;
+				return;
+			}
+		});
+	});
+	return ip;
+};
