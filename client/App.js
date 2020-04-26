c4463fd5513107eff3f6784ff5b670eabddf5bae
benf3632 <benf3632@gmail.com>
2020-04-26 15:23:28 +0300

    time picker working

--- client/App.js
+++ client/App.js
@@ -1,19 +1,45 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, Platform, Modal } from 'react-native';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+	const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+	const showDatePicker = () => {
+		setDatePickerVisibility(true);
+	};
+
+	const hideDatePicker = () => {
+		setDatePickerVisibility(false);
+	};
+
+	const handleConfirm = date => {
+		console.log('A date has been picked: ', date);
+		hideDatePicker();
+	};
+
+	return (
+		<View style={styles.container}>
+			<Button title='Show Time Picker' onPress={showDatePicker} />
+			<DateTimePickerModal
+				isVisible={isDatePickerVisible}
+				mode='time'
+				onConfirm={handleConfirm}
+				onCancel={hideDatePicker}
+			/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	picker: {
+		width: '100%',
+		height: 200,
+	},
 });
