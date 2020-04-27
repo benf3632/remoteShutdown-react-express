3f22401be9f7397a0281975ed6a0614249488b1d
benf3632 <benf3632@gmail.com>
2020-04-27 14:07:30 +0300

    added an ip text input

--- client/screens/MainScreen.js
+++ client/screens/MainScreen.js
@@ -1,10 +1,19 @@
 import React, { useState } from 'react';
-import { View, Button, StyleSheet } from 'react-native';
+import {
+	View,
+	Button,
+	StyleSheet,
+	Text,
+	TextInput,
+	Dimensions,
+} from 'react-native';
 import DateTimePickerModal from 'react-native-modal-datetime-picker';
+import Card from '../components/Card';
 
 const MainScreen = props => {
 	const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
-	const { selectedIp } = props.route.params;
+	const [time, setTime] = useState();
+	const [selectedIp, setSelectedIp] = useState(props.route.params.selectedIp);
 
 	const showTimePicker = () => {
 		setDatePickerVisibility(true);
@@ -20,24 +29,52 @@ const MainScreen = props => {
 	};
 
 	return (
-		<View style={styles.container}>
-			<Button title='Show Time Picker' onPress={showTimePicker} />
-			<DateTimePickerModal
-				isVisible={isDatePickerVisible}
-				mode='time'
-				onConfirm={handleConfirm}
-				onCancel={hideDatePicker}
-			/>
+		<View style={styles.screen}>
+			<Card style={styles.container}>
+				<Text style={styles.ipText}>Selcted Ip</Text>
+				<TextInput
+					style={styles.input}
+					placeholder='Enter an Ip'
+					value={selectedIp}
+					onChangeText={text => setSelectedIp(text)}
+					keyboardType='decimal-pad'
+					maxLength={15}
+				/>
+				<Text></Text>
+				<DateTimePickerModal
+					isVisible={isDatePickerVisible}
+					mode='time'
+					onConfirm={handleConfirm}
+					onCancel={hideDatePicker}
+				/>
+			</Card>
 		</View>
 	);
 };
 
 const styles = StyleSheet.create({
-	container: {
+	screen: {
 		flex: 1,
 		backgroundColor: '#fff',
 		alignItems: 'center',
 		justifyContent: 'center',
+	},
+	container: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		width: Dimensions.get('window').width - Dimensions.get('window').width / 4,
+		height: Dimensions.get('window').height / 3,
+	},
+	ipText: {
+		fontWeight: 'bold',
+		fontSize: 16,
+	},
+	input: {
+		width: 150,
+		marginVertical: 10,
+		textAlign: 'center',
+		borderBottomColor: '#ccc',
+		borderBottomWidth: 1,
 	},
 });
 
