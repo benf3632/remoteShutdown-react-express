4af8efabd01f29d787f156f650c2ff36dbbff8f2
CookieS <cookies@CookieSs-iMac.lan>
2020-04-29 02:49:26 +0300

    client: finished the MainScreen

--- client/screens/MainScreen.js
+++ client/screens/MainScreen.js
@@ -1,81 +1,155 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
-	View,
-	Button,
-	StyleSheet,
-	Text,
-	TextInput,
-	Dimensions,
+  View,
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  Dimensions,
+  TouchableOpacity,
 } from 'react-native';
 import DateTimePickerModal from 'react-native-modal-datetime-picker';
+import RadioForm from 'react-native-simple-radio-button';
+import Toast from 'react-native-easy-toast';
+
 import Card from '../components/Card';
 
+const radio_props = [
+  { label: 'Shutdown', value: 0 },
+  { label: 'Restart', value: 1 },
+  { label: 'Sleep', value: 2 },
+];
+
 const MainScreen = props => {
-	const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
-	const [time, setTime] = useState();
-	const [selectedIp, setSelectedIp] = useState(props.route.params.selectedIp);
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+  const [time, setTime] = useState(new Date(2000, 12, 12, 0, 0, 0));
+  const [selectedIp, setSelectedIp] = useState(props.route.params.selectedIp);
+  const [mode, setMode] = useState(0);
+  const [started, setStarted] = useState();
+  const [toast, setToast] = useState();
 
-	const showTimePicker = () => {
-		setDatePickerVisibility(true);
-	};
+  const showTimePicker = () => {
+    setDatePickerVisibility(true);
+  };
 
-	const hideDatePicker = () => {
-		setDatePickerVisibility(false);
-	};
+  const hideTimePicker = () => {
+    setDatePickerVisibility(false);
+  };
 
-	const handleConfirm = date => {
-		console.log('A date has been picked: ', date);
-		hideDatePicker();
-	};
+  const handleConfirm = date => {
+    setDatePickerVisibility(false);
+    const dateObj = new Date(date);
+    setTime(dateObj);
+  };
 
-	return (
-		<View style={styles.screen}>
-			<Card style={styles.container}>
-				<Text style={styles.ipText}>Selcted Ip</Text>
-				<TextInput
-					style={styles.input}
-					placeholder='Enter an Ip'
-					value={selectedIp}
-					onChangeText={text => setSelectedIp(text)}
-					keyboardType='decimal-pad'
-					maxLength={15}
-				/>
-				<Text></Text>
-				<DateTimePickerModal
-					isVisible={isDatePickerVisible}
-					mode='time'
-					onConfirm={handleConfirm}
-					onCancel={hideDatePicker}
-				/>
-			</Card>
-		</View>
-	);
+  const startTimerHandler = async () => {
+    const miliseconds = time.getHours() * 3600000 + time.getMinutes() * 60000;
+    const res = await fetch(`http://${selectedIp}:3000/execute`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        time: miliseconds,
+        mode: mode,
+      }),
+    });
+    if ((await res.status) === 200) {
+      setStarted(true);
+      toast.show('The timer has started');
+    } else {
+      toast.show('Could not start the timer');
+    }
+  };
+
+  const stopTimerHandler = async () => {
+    const res = await fetch(`http://${selectedIp}:3000/cancel`);
+    if ((await res.status) === 200) {
+      toast.show('The timer has stopped');
+      setStarted(false);
+    } else {
+      toast.show('Could not stop the timer');
+    }
+  };
+
+  return (
+    <View style={styles.screen}>
+      <Card style={styles.container}>
+        <Text style={styles.ipText}>Selected IP</Text>
+        <TextInput
+          style={styles.input}
+          placeholder='Enter an Ip'
+          value={selectedIp}
+          onChangeText={text => setSelectedIp(text)}
+          keyboardType='decimal-pad'
+          maxLength={15}
+        />
+        <Text style={styles.ipText}>Select Time</Text>
+        <TouchableOpacity onPress={showTimePicker} activeOpacity={0.2}>
+          <Text>
+            {time.getHours().toString().padStart(2, '0')}:
+            {time.getMinutes().toString().padStart(2, '0')}
+          </Text>
+        </TouchableOpacity>
+        <DateTimePickerModal
+          isVisible={isDatePickerVisible}
+          mode='time'
+          date={time}
+          onConfirm={handleConfirm}
+          onCancel={hideTimePicker}
+        />
+        <RadioForm
+          style={styles.radioForm}
+          formHorizontal={true}
+          labelHorizontal={false}
+          radio_props={radio_props}
+          initial={0}
+          onPress={val => setMode(val)}
+          buttonColor='#dd3b8a'
+          selectedButtonColor='#dd3b8a'
+        />
+        <Button
+          title={started ? 'Stop Timer' : 'Start Timer'}
+          color={'#5119a0'}
+          onPress={started ? stopTimerHandler : startTimerHandler}
+        />
+      </Card>
+      <Toast
+        ref={component => setToast(component)}
+        position='bottom'
+        positionValue={200}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-	screen: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	container: {
-		justifyContent: 'center',
-		alignItems: 'center',
-		width: Dimensions.get('window').width - Dimensions.get('window').width / 4,
-		height: Dimensions.get('window').height / 3,
-	},
-	ipText: {
-		fontWeight: 'bold',
-		fontSize: 16,
-	},
-	input: {
-		width: 150,
-		marginVertical: 10,
-		textAlign: 'center',
-		borderBottomColor: '#ccc',
-		borderBottomWidth: 1,
-	},
+  screen: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: Dimensions.get('window').width - Dimensions.get('window').width / 4,
+    height: Dimensions.get('window').height / 3,
+  },
+  ipText: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  input: {
+    width: 150,
+    marginVertical: 10,
+    textAlign: 'center',
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+  },
+  radioForm: {
+    marginVertical: 10,
+  },
 });
 
 export default MainScreen;
--- client/screens/SelectHostScreen.js
+++ client/screens/SelectHostScreen.js
@@ -1,164 +1,166 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import {
-	StyleSheet,
-	View,
-	ActivityIndicator,
-	Text,
-	FlatList,
-	Button,
-	RefreshControl,
-	TouchableOpacity,
-	ScrollView,
+  StyleSheet,
+  View,
+  ActivityIndicator,
+  Text,
+  FlatList,
+  Button,
+  RefreshControl,
+  TouchableOpacity,
+  ScrollView,
 } from 'react-native';
 import Card from '../components/Card';
 import TouchableNativeFeedback from '@expo/react-native-touchable-native-feedback-safe';
 
 const timeout = (ms, promise) => {
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			reject(new Error('timeout'));
-		}, ms);
-		promise.then(resolve, reject);
-	});
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(new Error('timeout'));
+    }, ms);
+    promise.then(resolve, reject);
+  });
 };
 
 const getHosts = async () => {
-	let hosts = [];
-	for (let i = 0; i < 256; i++) {
-		try {
-			const response = await timeout(
-				25,
-				fetch(`http://192.168.1.${i}:3000/host`)
-			);
-			const host = await response.json();
-			hosts = hosts.concat(host);
-		} catch (err) {}
-	}
-	return hosts;
+  let hosts = [];
+  for (let i = 0; i < 256; i++) {
+    try {
+      const response = await timeout(
+        25,
+        fetch(`http://192.168.1.${i}:3000/host`)
+      );
+      const host = await response.json();
+      hosts = hosts.concat(host);
+    } catch (err) {}
+  }
+  return hosts;
 };
 
 const SelectHostScreen = props => {
-	const [isLoading, setIsLoading] = useState(false);
-	const [isRefreshing, setIsRefreshing] = useState(false);
-	const [hosts, setHosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isRefreshing, setIsRefreshing] = useState(false);
+  const [hosts, setHosts] = useState([]);
 
-	useEffect(() => {
-		setIsLoading(true);
-		detectHosts().then(() => setIsLoading(false));
-	}, [detectHosts]);
+  useEffect(() => {
+    setIsLoading(true);
+    detectHosts().then(() => setIsLoading(false));
+  }, [detectHosts]);
 
-	const detectHosts = useCallback(async () => {
-		setIsRefreshing(true);
-		try {
-			const res = await getHosts();
-			setHosts(res);
-			setIsRefreshing(false);
-		} catch (err) {
-			console.log(err.message);
-		}
-		setIsRefreshing(false);
-	}, [setIsLoading]);
+  const detectHosts = useCallback(async () => {
+    setIsRefreshing(true);
+    try {
+      const res = await getHosts();
+      setHosts(res);
+      setIsRefreshing(false);
+    } catch (err) {
+      console.log(err.message);
+    }
+    setIsRefreshing(false);
+  }, [setIsLoading]);
 
-	useEffect(() => {
-		const willFocusSub = props.navigation.addListener('willFocus', detectHosts);
-		return () => {
-			willFocusSub.remove();
-		};
-	}, [detectHosts]);
+  useEffect(() => {
+    const willFocusSub = props.navigation.addListener('willFocus', detectHosts);
+    return () => {
+      willFocusSub.remove();
+    };
+  }, [detectHosts]);
 
-	const selectHostHandler = ip => {
-		props.navigation.navigate('Main', {
-			selectedIp: ip,
-		});
-	};
+  const selectHostHandler = ip => {
+    props.navigation.navigate('Main', {
+      selectedIp: ip,
+    });
+  };
 
-	if (isLoading) {
-		return (
-			<View style={{ ...styles.centered, marginVertical: 10 }}>
-				<Text style={{ marginVertical: 10 }}>Loading..</Text>
-				<ActivityIndicator size='large' />
-			</View>
-		);
-	}
+  if (isLoading) {
+    return (
+      <View style={{ ...styles.centered, marginVertical: 10 }}>
+        <Text style={{ marginVertical: 10 }}>Loading..</Text>
+        <ActivityIndicator color='#9519a0' size='large' />
+      </View>
+    );
+  }
 
-	if (!isLoading && hosts.length === 0) {
-		return (
-			<ScrollView
-				contentContainerStyle={styles.centered}
-				refreshControl={
-					<RefreshControl refreshing={isRefreshing} onRefresh={detectHosts} />
-				}
-			>
-				<View style={styles.centered}>
-					<Text>Did not found any hosts that running the server...</Text>
-					<Button
-						title='Try Entering Ip Manualy'
-						onPress={selectHostHandler('')}
-					/>
-				</View>
-			</ScrollView>
-		);
-	}
+  if (!isLoading && hosts.length === 0) {
+    return (
+      <ScrollView
+        contentContainerStyle={styles.centered}
+        refreshControl={
+          <RefreshControl
+            colors='#9519a0'
+            refreshing={isRefreshing}
+            onRefresh={detectHosts}
+          />
+        }>
+        <View style={styles.centered}>
+          <Text>Did not found any hosts that running the server...</Text>
+          <Button
+            title='Try Entering IP Manualy'
+            onPress={selectHostHandler.bind(this, '')}
+          />
+        </View>
+      </ScrollView>
+    );
+  }
 
-	return (
-		<FlatList
-			contentContainerStyle={styles.list}
-			onRefresh={detectHosts}
-			refreshing={isRefreshing}
-			data={hosts}
-			keyExtractor={item => item.hostname}
-			renderItem={item => (
-				<Card style={styles.hostContainer}>
-					<TouchableNativeFeedback
-						onPress={selectHostHandler.bind(this, item.item.ip)}
-						useForeground
-					>
-						<View style={styles.touchable}>
-							<Text style={styles.host}>{item.item.hostname}</Text>
-							<Text style={styles.ip}>{item.item.ip}</Text>
-						</View>
-					</TouchableNativeFeedback>
-				</Card>
-			)}
-		/>
-	);
+  return (
+    <FlatList
+      contentContainerStyle={styles.list}
+      onRefresh={detectHosts}
+      refreshing={isRefreshing}
+      data={hosts}
+      keyExtractor={item => item.hostname}
+      renderItem={item => (
+        <Card style={styles.hostContainer}>
+          <TouchableNativeFeedback
+            onPress={selectHostHandler.bind(this, item.item.ip)}
+            useForeground>
+            <View style={styles.touchable}>
+              <Text style={styles.host}>{item.item.hostname}</Text>
+              <Text style={styles.ip}>{item.item.ip}</Text>
+            </View>
+          </TouchableNativeFeedback>
+        </Card>
+      )}
+    />
+  );
 };
 
 const styles = StyleSheet.create({
-	centered: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-	},
-	list: {
-		justifyContent: 'center',
-		alignItems: 'center',
-	},
-	hostContainer: {
-		marginVertical: 20,
-		width: 300,
-		height: 100,
-		overflow: 'hidden',
-	},
-	touchable: {
-		borderRadius: 10,
-		overflow: 'hidden',
-		width: '100%',
-		height: '100%',
-		justifyContent: 'center',
-		alignItems: 'center',
-	},
-	host: {
-		fontWeight: 'bold',
-		fontSize: 16,
-		marginVertical: 10,
-		textAlign: 'center',
-	},
-	ip: {
-		color: '#ccc',
-		fontSize: 13,
-		textAlign: 'center',
-	},
+  centered: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  list: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  hostContainer: {
+    marginVertical: 20,
+    width: 300,
+    height: 100,
+    overflow: 'hidden',
+  },
+  touchable: {
+    borderRadius: 10,
+    overflow: 'hidden',
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  host: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginVertical: 10,
+    textAlign: 'center',
+  },
+  ip: {
+    color: '#ccc',
+    fontSize: 13,
+    textAlign: 'center',
+  },
 });
 
 export default SelectHostScreen;
