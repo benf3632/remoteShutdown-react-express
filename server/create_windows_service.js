eee40ad44dd8fb3920451c2f6b24ecad931fd95c
benf3632 <benf3632@gmail.com>
2021-02-06 04:15:17 +0200

    fixed windows service creator

--- server/create_windows_service.js
+++ server/create_windows_service.js
@@ -3,7 +3,7 @@ var Service = require('node-windows').Service;
 var svc = new Service({
   name: 'Remote Shutdown',
   description: 'Runs the remote shutdown server',
-  script: 'C:\\Users/CookieS/dev/remoteShutdown/server/src/index.js',
+  script: 'C:\\Users\\CookieS\\Documents\\dev\\remoteShutdown-react-express\\server\\src\\index.js',
 });
 
 svc.on('install', () => {
